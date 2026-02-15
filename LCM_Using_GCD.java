5cabd923c370e5c4bb0f58c92fe224268f2c6d73
Vishwajeet Patil <vishwajeetpatil7648@gmail.com>
2026-02-15 12:47:33 +0530

    LCM using GCD

---
+++ LCM_Using_GCD.java
@@ -0,0 +1,33 @@
+package java_number_problems;
+
+import java.util.Scanner;
+
+public class LCM_Using_GCD {
+
+    // Function to find GCD
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Function to find LCM
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("LCM = " + lcm(a, b));
+    }
+}
